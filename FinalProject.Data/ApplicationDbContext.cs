a3db30cc57b248317fb932909487acf4760b2549
Hannah Young <hamyoun@iu.edu>
2022-06-22 19:30:18 -0400

    init commit

---
+++ FinalProject.Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+    {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+    }
