4006337e4bef2d7122db00df5295339bd35d9712
Zackery Kramer <zackkramer@Zackerys-Air.attlocal.net>
2022-06-23 18:17:24 -0400

    FirstDataPush

--- FinalProject.Data/Entities/Customer.cs
+++ FinalProject.Data/Entities/Customer.cs
@@ -6,5 +6,9 @@ using System.Threading.Tasks;
 
     public class Customer
     {
-        
+       
+       [key]
+       public int Id { get; set; }
+       public string FirstName { get; set; }
+       public string LastName { get; set; }
     }
