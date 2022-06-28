1dcbfe2e1c2b0fe8599737b1f0cfee76ef9801e8
Hannah Young <hamyoun@iu.edu>
2022-06-28 18:05:56 -0400

    CRUD for kitchen invetory and MOdels

---
+++ FinalProject.Models/Models/InventoryUpdate.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+    public class InventoryUpdate
+    {
+        public int ID { get; set; }
+        public string ItemsName { get; set; }
+
+        public ItemCategory Category  { get; set; }
+        public int Amount { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
--- FinalProject.Services/Services/InventoryService.cs
+++ FinalProject.Services/Services/InventoryService.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FinalProject.Data;
+using Microsoft.EntityFrameworkCore;
 
-  public class InventoryService : IInventoryService
+public class InventoryService : IInventoryService
     {
         private readonly ApplicationDbContext _context;
 
@@ -18,10 +20,50 @@ using System.Threading.Tasks;
                 ExpirationDate = model.ExpirationDate
 
             };
-            //_context.Items.Add(item);
+            _context.Items.Add(item);
             var numberOfChanges = await _context.SaveChangesAsync();
             return numberOfChanges == 1;
         }
+
+        public async Task<IEnumerable<InventoryListItem>> GetInventoryListItemsAsync()
+        {
+            var items = await _context.Items
+            .Select(entity => new InventoryListItem
+            {
+                ID = entity.ID,
+                ItemsName = entity.ItemsName,
+                ExpirationDate = entity.ExpirationDate
+            })
+            .ToListAsync();
+        return items;
+        }
+        public async Task<bool> UpdateInventoryAsync(InventoryUpdate request)
+        {
+            var inventoryEntity = await _context.Items.FindAsync(request.ID);
+            if(inventoryEntity != null)
+            {
+                inventoryEntity.ItemsName = request.ItemsName;
+                inventoryEntity.Amount = request.Amount;
+                inventoryEntity.Category = request.Category;
+                inventoryEntity.ExpirationDate = request.ExpirationDate;
+
+                var numberOfChanges = await _context.SaveChangesAsync();
+                return numberOfChanges == 1;
+            }
+            return false;
+        }
+        public async Task<bool> DeleteInventoryItemAsync(int itemId)
+        {
+            var inventoryEntity = await _context.Items.FindAsync(itemId);
+            if(itemId != null)
+            {
+                _context.Items.Remove(inventoryEntity);
+                return await _context.SaveChangesAsync() ==1;
+            }
+            return false;
+        }
+
+
 
 
     }
