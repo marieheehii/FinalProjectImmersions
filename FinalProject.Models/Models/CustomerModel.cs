3f539aff0fb3357e3f9dd120b0279fc38edc5a03
Zackery Kramer <zackkramer@Zackerys-Air.attlocal.net>
2022-06-28 18:01:34 -0400

    Zack Service Push, CRUD for Grocery still bugged

--- FinalProject.Models/Models/CustomerModel.cs
+++ FinalProject.Models/Models/CustomerModel.cs
@@ -6,5 +6,7 @@ using System.Threading.Tasks;
 
     public class CustomerModel
     {
-        
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
--- FinalProject.Services/Services/CustomerService.cs
+++ FinalProject.Services/Services/CustomerService.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FinalProject.Services.Services
-{
-    public class CustomerService
+
+    public class CustomerService : ICustomerService
     {
-        
+        private readonly ApplicationDbContext _context;
+        public CustomerServices(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        private readonly int _customerID;
+
+        public async Task<IEnumerable<CustomerListItem>> GetCustomerListItemsAsync()
+        {
+            var customers = await _context.Characters
+            .Select(entity => new CharacterListItem
+            {
+                Id = entity.ID,
+                FirstName = entity.FirstName,
+                LastName = entity.LastName
+            })
+            .ToListAsync();
+            return customers;
+        }
+
+        public async Task<bool> CreateCustomerAsync(CustomerModel model)
+        {
+            var customer = new Character
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName, 
+            };
+
+            _context.Customers.Add(customer);
+        }
+
+        public async Task<CustomerDelete> DeleteCustomerDetailsAsync(int id)
+        {
+            Console.Clear();
+            Console.WriteLine("Please Enter the Customer's ID");
+            int Input = int.Parse(Console.ReadLine());
+            if (_context.DeleteCustomerDetailsAsync(userInput))
+            {
+                System.Console.WriteLine("Customer was Successfully Deleted");
+            }
+            else
+            {
+                System.Console.WriteLine("Sorry, Customer was not Deleted.");
+            }
+            PressAnyKeyToContinue();
+        }
     }
-}
