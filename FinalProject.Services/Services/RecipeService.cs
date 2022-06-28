45cd42e3620b557abde6a72023114ce8b2c6500f
Hannah Young <hamyoun@iu.edu>
2022-06-28 18:12:32 -0400

    premerge kitchen inventory

--- FinalProject.Services/Services/RecipeService.cs
+++ FinalProject.Services/Services/RecipeService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 public class RecipeService
 {
@@ -27,7 +28,7 @@ public class RecipeService
 
         return numberOfChanges == 1;
     }
-    public async Task<IEnumerable<RecipeListItem>> ListAllRecipes()
+    /*public async Task<IEnumerable<RecipeListItem>> ListAllRecipes()
     {
         var recipes = await _context.Recipes.ToListAsync();
         return Ok(recipes);
