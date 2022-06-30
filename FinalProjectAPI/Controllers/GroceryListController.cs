daa2eb119f8e807de34ef41d3ad91cd2589d06b6
Zackery Kramer <zackkramer@Zackerys-Air.attlocal.net>
2022-06-29 20:19:58 -0400

    AddedDebugger

--- FinalProjectAPI/Controllers/GroceryListController.cs
+++ FinalProjectAPI/Controllers/GroceryListController.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 
 
-    public class GroceryListController
+    [Route("[controller]")]
+    public class GroceryListController : ControllerBase
     {
-        
+        private readonly GListService listService;
+
+        public InventoryController 
+
     }
