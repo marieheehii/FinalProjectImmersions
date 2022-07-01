c0339fc8c03013552e60d48bd309e69e0fd47f43
hayzemke <hay.zemke@gmail.com>
2022-06-30 20:56:30 -0400

    develop3

--- FinalProject.Services/Services/InventoryService.cs
+++ FinalProject.Services/Services/InventoryService.cs
@@ -9,63 +9,66 @@ public class InventoryService : IInventoryService
 {
     private readonly ApplicationDbContext _context;
 
+    public InventoryService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
 
     public async Task<bool> CreateInventoryItemAsyc(KitchenInventoryModel model)
     {
-            var item = new KitchenInventory
-            {
-                ItemsName = model.ItemsName,
-                Amount = model.Amount,
-                Category = model.Category,
-                ExpirationDate = model.ExpirationDate
+        var item = new KitchenInventory
+        {
+            ItemsName = model.ItemsName,
+            Amount = model.Amount,
+            Category = model.Category,
+            ExpirationDate = model.ExpirationDate
 
-            };
-            _context.Items.Add(item);
+        };
+        _context.Items.Add(item);
+        var numberOfChanges = await _context.SaveChangesAsync();
+        return numberOfChanges == 1;
+    }
+
+    public async Task<IEnumerable<InventoryListItem>> GetInventoryListItemsAsync()
+    {
+        var items = await _context.Items
+        .Select(entity => new InventoryListItem
+        {
+            ID = entity.ID,
+            ItemsName = entity.ItemsName,
+            ExpirationDate = entity.ExpirationDate
+        })
+        .ToListAsync();
+        return items;
+    }
+    public async Task<bool> UpdateInventoryAsync(InventoryUpdate request)
+    {
+        var inventoryEntity = await _context.Items.FindAsync(request.ID);
+        if (inventoryEntity != null)
+        {
+            inventoryEntity.ItemsName = request.ItemsName;
+            inventoryEntity.Amount = request.Amount;
+            inventoryEntity.Category = request.Category;
+            inventoryEntity.ExpirationDate = request.ExpirationDate;
+
             var numberOfChanges = await _context.SaveChangesAsync();
             return numberOfChanges == 1;
+        }
+        return false;
     }
-
-        public async Task<IEnumerable<InventoryListItem>> GetInventoryListItemsAsync()
+    public async Task<bool> DeleteInventoryItemAsync(int itemId)
+    {
+        var inventoryEntity = await _context.Items.FindAsync(itemId);
+        if (itemId != null)
         {
-            var items = await _context.Items
-            .Select(entity => new InventoryListItem
-            {
-                ID = entity.ID,
-                ItemsName = entity.ItemsName,
-                ExpirationDate = entity.ExpirationDate
-            })
-            .ToListAsync();
-        return items;
-        }
-        public async Task<bool> UpdateInventoryAsync(InventoryUpdate request)
-        {
-            var inventoryEntity = await _context.Items.FindAsync(request.ID);
-            if(inventoryEntity != null)
-            {
-                inventoryEntity.ItemsName = request.ItemsName;
-                inventoryEntity.Amount = request.Amount;
-                inventoryEntity.Category = request.Category;
-                inventoryEntity.ExpirationDate = request.ExpirationDate;
-
-                var numberOfChanges = await _context.SaveChangesAsync();
-                return numberOfChanges == 1;
-            }
-            return false;
-        }
-        public async Task<bool> DeleteInventoryItemAsync(int itemId)
-        {
-            var inventoryEntity = await _context.Items.FindAsync(itemId);
-            if(itemId != null)
-            {
-                _context.Items.Remove(inventoryEntity);
-                return await _context.SaveChangesAsync() ==1;
-            }
-            return false;
             _context.Items.Remove(inventoryEntity);
-            return await _context.SaveChangesAsync()==1;
+            return await _context.SaveChangesAsync() == 1;
         }
-        
+        return false;
+        _context.Items.Remove(inventoryEntity);
+        return await _context.SaveChangesAsync() == 1;
     }
+}
 
 
 
--- FinalProjectAPI/Controllers/InventoryController.cs
+++ FinalProjectAPI/Controllers/InventoryController.cs
@@ -6,51 +6,53 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 
 
-    [Route("[controller]")]
-    [ApiController]
+[Route("[controller]")]
+[ApiController]
 
-    public class InventoryController : ControllerBase
+public class InventoryController : ControllerBase
+{
+    private readonly IInventoryService _service;
+
+    public InventoryController(IInventoryService service)
     {
-        private readonly IInventoryService _service;
-
-        public InventoryController(IInventoryService service)
-        {
-            _service = service;
-        }
-
-        [HttpPost("Register")]
-        public async Task<IActionResult> RegisterInventory([FromBody] KitchenInventoryModel model)
-        {
-            if(!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            var registerResult = await _service.CreateInventoryItemAsyc(model);
-            if (registerResult)
-            {
-                return Ok("Inventory was registered");
-            }
-            return BadRequest("Inventory could not be registered");
-        }
-        
-
-        [HttpGet]
-        public async Task<IActionResult> GetAllInventoryItems()
-        {
-            var customers = _service.GetInventoryListItemsAsync();
-            return Ok(customers);
-        }
-
-        public async Task<IActionResult> UpdateInventory([FromBody] InventoryUpdate request)
-    {
-        if(!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            return await _service.UpdateInventoryAsync(request)
-            ? Ok($"Inventory was successfully updated.")
-            : BadRequest($"Item could not be updated.");
+        _service = service;
     }
+
+    [HttpPost("Register")]
+    public async Task<IActionResult> RegisterInventory([FromForm] KitchenInventoryModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var registerResult = await _service.CreateInventoryItemAsyc(model);
+        if (registerResult)
+        {
+            return Ok("Inventory was registered");
+        }
+        return BadRequest("Inventory could not be registered");
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllInventoryItems()
+    {
+        var customers = _service.GetInventoryListItemsAsync();
+        return Ok(customers);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateInventory([FromForm] InventoryUpdate request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        return await _service.UpdateInventoryAsync(request)
+        ? Ok($"Inventory was successfully updated.")
+        : BadRequest($"Item could not be updated.");
+    }
+    
     [HttpDelete]
     [Route("{inventoryId:int}")]
     public async Task<IActionResult> DeleteInventory([FromRoute] int id)
@@ -59,4 +61,4 @@ using Microsoft.AspNetCore.Http;
         ? Ok($"Item {id} was deleted from your inventory successfully.")
         : BadRequest($"Item {id} could not be deleted.");
     }
-    }
+}
