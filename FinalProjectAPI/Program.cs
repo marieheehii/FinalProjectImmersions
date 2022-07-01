7e25386b920014154a2ed55db9dc106e384b7825
hayzemke <hay.zemke@gmail.com>
2022-06-30 20:46:48 -0400

    develop3

--- FinalProjectAPI/Program.cs
+++ FinalProjectAPI/Program.cs
@@ -14,12 +14,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-<<<<<<< HEAD
 builder.Services.AddScoped<ICustomerService, CustomerServices>();
 builder.Services.AddScoped<IRecipe, RecipeService>();
-=======
 builder.Services.AddScoped<IInventoryService,InventoryService>();
->>>>>>> 1bfe73a3bbf55c91f76af0863c681fada2af02d0
 // lines 3-19 (or when 'var app' appears) is our ioc container, which looksat how things are set up, change the ability of when and how things are set up.
 var app = builder.Build();
 
