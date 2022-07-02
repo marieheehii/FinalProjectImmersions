d58faa583d5687cc8c1c2408ec2c96f3888a6498
Hannah Young <hamyoun@iu.edu>
2022-07-01 23:56:41 -0400

    postmerge but pre merge conflict fix

--- FinalProjectAPI/Program.cs
+++ FinalProjectAPI/Program.cs
@@ -17,18 +17,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-<<<<<<< HEAD
 builder.Services.AddScoped<ICustomerService, CustomerServices>();
 builder.Services.AddScoped<IRecipe, RecipeService>();
 builder.Services.AddScoped<IInventoryService,InventoryService>();
 // lines 3-19 (or when 'var app' appears) is our ioc container, which looksat how things are set up, change the ability of when and how things are set up.
-=======
-// builder.Services.AddScoped<ICustomerService,CustomerService>();
-builder.Services.AddScoped<IInventoryService,InventoryService>();
-// builder.Services.AddTransient(new CustomerService());
-// ^The way this is written isn't wrong, just another way of writing it out
-// lines 3-when 'var app' appears is our ioc container, which looksat how things are set up, change the ability of when and how things are set up.
->>>>>>> c87e0be6093190fd3fc9200baf51efbf98320eae
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
